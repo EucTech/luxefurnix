3cef5ddec81a56bd522f6f7d56c5e61c490b49a7
EucTech <euccreative@gmail.com>
2023-11-28 16:47:24 +0100

    inti files

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 # For secret key
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'a2852701f7354a86b0a291cabaaf9df3'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///luxefurnix.db"
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
 
 
 from my_project import route
