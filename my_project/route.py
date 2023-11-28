cfe707d636b2311e5460faa7d772599b99010d48
EucTech <euccreative@gmail.com>
2023-11-28 20:42:59 +0100

    updated

--- my_project/route.py
+++ my_project/route.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from my_project import app, db, Bcrypt
+from my_project import app, db, bcrypt
 from my_project.forms import SignupForm, LoginForm
 from my_project.models import User
 
@@ -19,8 +19,10 @@ def register():
     form = SignupForm()
     # To check if the form is validate when submit is clicked
     if form.validate_on_submit():
+        # To hash the password
+        hash_pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(fullname=form.fullname.data, email=
-                    form.email.data, phone_number=form.phone_number.data, password=form.password.data)
+                    form.email.data, phone_number=form.phone_number.data, password=hash_pwd)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created!', 'success')
@@ -28,7 +30,7 @@ def register():
     return render_template("signup.html", title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
