486c7512d8aa78519414b282c139b2806978d20b
EucTech <euccreative@gmail.com>
2023-11-29 03:12:56 +0100

    add login models

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -12,6 +13,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///luxefurnix.db"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
+# when a user tries to access a protected resource without being
+# logged in, they will be redirected to the login endpoint
+login_manager.login_view = 'login'
+# This allows you to style or handle these messages differently in your
+# templates, such as showing them with a specific color or formatting.
+login_manager.login_message_category = 'info'
 
 from my_project import route
