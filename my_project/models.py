541f74fb3a7e03028f89668d2407dadcb77e1910
EucTech <euccreative@gmail.com>
2023-11-29 03:10:24 +0100

    update route to login user

--- my_project/models.py
+++ my_project/models.py
@@ -1,10 +1,25 @@
-from my_project import db
+from my_project import db, login_manager
 from datetime import datetime
 import uuid
-"""This is a file for database"""
+from flask_login import UserMixin
+"""
+    This is a file for database
+
+    The UserMixin class provides default implementations 
+    for methods is_authenticated, is_active, is_anonymous, 
+    and get_id to work seamlessly with Flask-Login
+"""
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    """This function is a flask_login method that
+        loads the Users ID from the database
+    """
+    return User.query.get(user_id)
+
+
+class User(db.Model, UserMixin):
     """Class for User data"""
     # The lambda function on the id ensure that a new id is created at every instance
     id = db.Column(db.String(36), default=lambda: str(uuid.uuid4()), primary_key=True, nullable=False)
