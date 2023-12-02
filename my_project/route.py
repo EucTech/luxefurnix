e6bdda51c8d992a226640659317cf4709589114e
EucTech <euccreative@gmail.com>
2023-12-02 22:26:32 +0100

    update

--- my_project/route.py
+++ my_project/route.py
@@ -1,8 +1,11 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from my_project import app, db, bcrypt
-from my_project.forms import SignupForm, LoginForm
-from my_project.models import User, Product, Category
-from flask_login import login_user, current_user, logout_user
+from my_project.forms import SignupForm, LoginForm, UploadProductForm
+from my_project.models import User, Product
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 with app.app_context():
@@ -12,9 +15,8 @@ with app.app_context():
 @app.route("/")
 @app.route("/home")
 def home():
-    product = Product()
-    category = Category()
-    return render_template("home.html")
+    products = Product.query.all()
+    return render_template("home.html", products=products)
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -61,7 +63,48 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/description", methods=['GET', 'POST'])
-def description():
-    """This is the route for description"""
-    return render_template('description.html')
+def save_pic(form_pic):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_pic.filename)
+    picture_fn = random_hex + f_ext
+    pic_path = os.path.join(app.root_path, 'static/images/products', picture_fn)
+
+    # To resize the image before uploading
+    output_size = (800, 800)
+    i = Image.open(form_pic)
+    i.thumbnail(output_size)
+
+    i.save(pic_path)
+
+    return picture_fn
+
+
+@app.route("/upload_products", methods=['GET', 'POST'])
+@login_required
+def upload_products():
+    """This is a route that uploads products"""
+    form = UploadProductForm()
+    if form.validate_on_submit():
+        image_filename = save_pic(form.product_image.data)
+
+        product = Product(
+            product_name=form.product_name.data,
+            product_desc=form.product_desc.data,
+            price=form.price.data,
+            color=form.color.data,
+            product_images=image_filename,
+            # category_id=
+        )
+
+        db.session.add(product)
+        db.session.commit()
+        flash('Product uploadded successfully', 'success')
+        return redirect(url_for('home'))
+    return render_template("upload_products.html", title='Upload Products', form=form)
+
+
+@app.route("/product/<string:product_id>", methods=['GET', 'POST'])
+def product(product_id):
+    """This is the route for products"""
+    product = Product.query.get_or_404(product_id)
+    return render_template('description.html', title=product.product_name, product=product)
