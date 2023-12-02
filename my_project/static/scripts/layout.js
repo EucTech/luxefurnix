954d139ff6e6b145dcf67ce7e74a11d74867d00b
EucTech <euccreative@gmail.com>
2023-12-02 22:26:11 +0100

    products

--- my_project/static/scripts/layout.js
+++ my_project/static/scripts/layout.js
@@ -55,3 +55,24 @@ function smNext() {
 
 showSlide(current);
 setInterval(smNext, 5000);
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
